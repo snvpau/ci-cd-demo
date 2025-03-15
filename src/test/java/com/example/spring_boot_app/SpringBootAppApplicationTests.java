8c7449e0e13687f770053ab3a81e6456719bad9b
snvpau <paugsandoval@gmail.com>
2025-03-14 18:48:36 -0600

    Guardando cambios antes de pull

---
+++ src/test/java/com/example/spring_boot_app/SpringBootAppApplicationTests.java
@@ -0,0 +1,30 @@
+package com.example.spring_boot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SpringBootAppApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testHelloEndpoint() throws Exception {
+        // Realiza una solicitud GET a la ruta "/"
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk()) 
+                .andExpect(content().string("Hello World!")); 
+    }
+}
