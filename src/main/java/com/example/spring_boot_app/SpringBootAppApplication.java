ba1559aecbda04a243a4424904fb81ff82dc6927
snvpau <84196376+snvpau@users.noreply.github.com>
2025-03-14 19:32:36 -0600

    Update SpringBootAppApplication.java
    comments

--- src/main/java/com/example/spring_boot_app/SpringBootAppApplication.java
+++ src/main/java/com/example/spring_boot_app/SpringBootAppApplication.java
@@ -3,10 +3,10 @@ package com.example.spring_boot_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //aplicacion de spring boot
 public class SpringBootAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootAppApplication.class, args);
+        SpringApplication.run(SpringBootAppApplication.class, args); //se inicia la aplicacion spring boot
     }
 }
